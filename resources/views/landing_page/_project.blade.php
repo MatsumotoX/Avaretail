601509d88a6ae823f1b59653759110464413688d
MatsumotoX <pongsakorn.rp@gmail.com>
2019-03-02 17:58:48 +0700

    Nature of Machines

--- resources/views/landing_page/_project.blade.php
+++ resources/views/landing_page/_project.blade.php
@@ -16,22 +16,16 @@
                     </a>
                     <p class="text-blog">Humans have a huge prior knowledge</p>
                     <p class="text-blog">Humans learn from rules (knowledge)</p>
-                    <div class="caption text-center">
-                        <a class="btn btn-default more-team-view-all" id="White Paper Top" href="https://arbot.cryptovation.co/" target="_blank"
-                            role="button">More Info</a>
-                    </div>
+                    
                 </div>
                 <div class="col-xs-12 col-sm-6 portfolio-box mt10 mb10 rightReveal">
                     <h5 class="heading-1-x what">Robot Trading</h5>
                     <a href="/ava" id="ava-advisor-image" target="_blank">
                         <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/Ava44x.png" id="ava-advisor-image" alt="4" class="img-responsive portfolio-image existing-project">
                     </a>
-                    <p class="text-blog">The AVA Advisor team has developed a state of the art artificial intelligence powered, auto-trading portfolio
-                        allocation platform to provide execution and re-weighting advise on Chinese and Thailand public market
-                        equities to maximize investment returns.</p>
-                    <div class="caption text-center">
-                        <a class="btn btn-default more-team-view-all" id="White Paper Top" href="/ava" target="_blank" role="button">More Info</a>
-                    </div>
+                    <p class="text-blog">Machines don't have a huge prior knowledge</p>
+                    <p class="text-blog">Machines learn from raw data</p>
+                   
                 </div>
             </div>
 
